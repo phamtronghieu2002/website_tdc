d2f46c455147de4ff6ba17adaed81e78bd924b68
phducdung <phamducdung147@gmail.com>
2024-08-02 10:45:35 +0700

    pull

--- src/public/utils/lang.js
+++ src/public/utils/lang.js
@@ -1,21 +1,98 @@
 const dataLang = {
     en: {
         home: "Home",
-        about: "About",
+        about: "Introduction",
         solutions: "Solutions",
         news: "News",
         contact: "Contact",
-        login_title:"Login",
-        header:"driver"
+        login_title: "Login",
+        // footer
+        footer_lienhe: "CONTACT",
+        footer_chinhsach: "POLICY",
+        footer_bando: "MAP",
+        //news
+        news_tinnoibat: "Featured News",
+        //contact
+        contact_header: "Connect with us",
+        contact_goingay: "Call now",
+        contact_bando: "Map",
+        contact_lienlac: "Contact",
+        contact_diachi: "Address",
+        contact_gui: "Send",
+        //solution
+        solutions_giaiphap: "SOLUTION",
+        solutions_cuachungtoi: "ours",
+        solutions_tinhnang: "FEATURES",
+        solutions_thongso: "TECHNICAL SPECIFICATIONS",
+        solutions_dangky: "REGISTER FOR CONSULTATION",
+        solutions_tuvan: "Register for consultation",
+        solutions_gui: "Send",
+        // introduce
+        intro_header: "Technology Driver - TDC",
+        intro_content_sec_text1: "Technology Driver Corporation - Technology Driver Corporation was established by a dynamic, highly qualified, enthusiastic, and professionally trained team. We are imbued with the motto 'Customer-centric' to serve the best and constantly improve and research to find solutions to solve all problems of the Transportation industry.",
+        intro_content_sec_text2: "TD CORP offers customers 3 main product lines: 5G GPS journey monitoring management device with eSIM; Dash cam; Surveillance camera according to Decree 10. In particular, we are proud to be the pioneer in providing eSIM journey monitoring management devices with outstanding features compared to traditional SIM cards.",
+        intro_content_sec_text3: "TD CORP believes that it will become an 'Indispensable Companion' of customers with outstanding developments in products and with the professional attitude of TDC staff.",
+        intro_tam_nhin: "VISION",
+        intro_content_tam_nhin: "TDC is determined and constantly strives to reach the aspiration of becoming 'The leading company in modern technology equipment in the field of Transportation'.",
+        intro_su_menh: "MISSION",
+        intro_content_su_menh: "'Taking customers as the center, taking prestige as the purpose of development and responsible business'. TD CORP always listens to understand each customer's needs, constantly researching transportation management solutions to help optimize profits for customers.",
+        intro_giatri: "CORE VALUES",
+        intro_xuhuong: "Trends",
+        intro_content_xuhuong: "Continuously updating, catching up with new trends of the 4.0 era, researching and developing high-tech products and superior solutions to meet customer needs.",
+        intro_conghien: "Dedication",
+        intro_content_conghien: "With the motto 'Dedication, work hard', perfecting every day to become the 'best version' to bring experiences and serve customers beyond expectations.",
+        intro_sangtao: "Creation",
+        intro_content_sangtao: "Creation, constant innovation, creating differences in each product with a breakthrough in technology."
+        
+
+
+
+
     },
     vi: {
+        //header
         home: "Trang chủ",
         about: "Giới thiệu",
         solutions: "Giải pháp",
         news: "Tin tức",
         contact: "Liên hệ",
-        login_title:"Đăng nhập",
-        header:"tai xe"
-
+        login_title: "Đăng nhập",
+        // footer
+        footer_lienhe: "LIÊN HỆ",
+        footer_chinhsach: "CHÍNH SÁCH",
+        footer_bando: "BẢN ĐỒ",
+        //news
+        news_tinnoibat:"Tin tức nổi bật",
+        //contact
+        contact_header: "Kết nối với chúng tôi",
+        contact_goingay: "Gọi ngay",
+        contact_bando: "Bản Đồ",
+        contact_lienlac: "Liên Lạc",
+        contact_diachi: "Địa chỉ",
+        contact_gui: "Gửi",
+        //solution
+        solutions_giaiphap:"GIẢI PHÁP",
+        solutions_cuachungtoi:"của chúng tôi",
+        solutions_tinhnang:"TÍNH NĂNG",
+        solutions_thongso:"THÔNG SỐ KỸ THUẬT",
+        solutions_dangky:"ĐĂNG KÝ TƯ VẤN",
+        solutions_tuvan:"Đăng ký nhận tư vấn",
+        solutions_gui:"Gửi",
+        // introduce
+        intro_header: "Tài xế công nghệ - TDC",
+        intro_content_sec_text1: "Tài xế công nghệ - Technology Driver Corporation được thành lập từ đội ngũ năng động, trình độ chuyên môn cao, nhiệt huyết và được đào tạo chuyên nghiệp. Chúng tôi thấm nhuần phương châm “Lấy khách hàng làm trọng tâm” để phục vụ tốt nhất và không ngừng cải tiến, nghiên cứu để tìm ra giải pháp giải quyết mọi vấn đề của ngành Vận tải.",
+        intro_content_sec_text2: "TD CORP đem đến cho khách hàng 3 dòng sản phẩm chính: Thiết bị quản lý giám sát hành trình 5G GPS với eSIM; Camera hành trình; Camera giám sát theo nghị định 10. Đặc biệt, chúng tôi tự hào là đơn vị tiên phong cung cấp Thiết bị quản lý giám sát hành trình với eSIM cùng những tính năng vượt trội so với sim truyền thống.",
+        intro_content_sec_text3: "TD CORP tin tưởng mình sẽ trở thành “Người bạn đồng hành không thể thiếu” của các khách hàng bằng những bước phát triển vượt bậc trong sản phẩm và bằng thái độ chuyên nghiệp của đội ngũ nhân viên TDC.",
+        intro_tam_nhin: "TẦM NHÌN",
+        intro_content_tam_nhin: "TDC quyết tâm và phấn đấu không ngừng để vươn đến khát vọng trở thành “Công ty dẫn đầu về thiết bị công nghệ hiện đại trong lĩnh vực Giao thông vận tải”.",
+        intro_su_menh: "SỨ MỆNH",
+        intro_content_su_menh: "“Lấy khách hàng làm trọng tâm, lấy uy tín làm mục đích phát triển và kinh doanh trách nhiệm”. TD CORP luôn lắng nghe để thấu hiểu từng nhu cầu của Quý khách hàng, nỗ lực nghiên cứu không ngừng những giải pháp quản lý vận tải giúp tối ưu lợi nhuận cho khách hàng.",
+        intro_giatri: "GIÁ TRỊ CỐT LÕI",
+        intro_xuhuong: "Xu hướng",
+        intro_content_xuhuong: "Liên tục cập nhật, bắt kịp xu hướng mới của thời đại 4.0, nghiên cứu và pháttriển những sản phẩm công nghệ cao và những giải pháp ưu việt đáp ứng nhu cầu của khách hàng.",
+        intro_conghien: "Cống hiến",
+        intro_content_conghien: "Với phương châm “Cống hiến hết mình, làm việc hết sức”, hoàn thiện mỗi ngày trở thành “phiên bản tốt nhất” để đem lại những trải nghiệm và phục vụ khách hàng hơn cả sự mong đợi.",
+        intro_sangtao: "Sáng tạo",
+        intro_content_sangtao: "Sáng tạo, đổi mới không ngừng, tạo sự khác biệt trong từng sản phẩm bằng những bước tiến đột phá về công nghệ.",
     }
 };
